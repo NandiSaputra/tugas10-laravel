a325cf82ffe696a3766e3241cef7b5acb8de70f0
Nandi Saputra <nandysyahputra59@gmail.com>
2022-01-12 22:05:30 +0700

    commit

--- resources/views/groups/addmember.blade.php
+++ resources/views/groups/addmember.blade.php
@@ -1,15 +1,51 @@
-<form action="/groups/addmember/{{ $group->id }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Nama Teman</label>
-        <select class="form-select" aria-label="Default select example" name="friend_id">
-            <option selected>PILIH TEMAN</option>
-            @foreach ($friend as $f)
-                <option value="{{ $f->id }}">{{ $f->nama }}</option>
-            @endforeach
-        </select>
-    </div>
-    <button type="submit" class="btn btn-primary">Tambah ke Group</button>
-    <a href="/groups" class="btn btn-danger">kembali</a>
-</form>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Tambah Anggota</title>
+</head>
+
+<body>
+    <form action="/groups/addmember/{{ $group->id }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nama Teman</label>
+            <select class="form-select" aria-label="Default select example" name="friend_id">
+                <option selected>PILIH TEMAN</option>
+                @foreach ($friend as $f)
+                    <option value="{{ $f->id }}">{{ $f->nama }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Tambah ke Group</button>
+        <a href="/groups" class="btn btn-danger">kembali</a>
+    </form>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    -->
+</body>
+
+</html>
